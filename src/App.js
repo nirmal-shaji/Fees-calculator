958d0f74487f1baa92719faf118797ed8eb2dced
Nirmal Shaji Joseph <nirmalshaji789@gmail.com>
2023-02-04 19:07:53 +0530

    style updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Form from './components/Form';
-import './App.css'
+
 
 
 
 const App = () => {
   return (
-   <div className='app'>
+   <>
          <Form/>   
-   </div>
+   </>
   
  
   )
